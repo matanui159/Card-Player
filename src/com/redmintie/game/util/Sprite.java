33710125f2868855833f5a3c9bca8f6e321ea8b2
Joshua Minter <josh@minteronline.com>
2016-01-03 21:55:39 +1000

    Improved sprite loading

--- src/com/redmintie/game/util/Sprite.java
+++ src/com/redmintie/game/util/Sprite.java
@@ -23,12 +23,14 @@ import static org.lwjgl.opengl.GL11.glVertex2d;
 import static org.lwjgl.opengl.GL12.GL_CLAMP_TO_EDGE;
 import static org.lwjgl.stb.STBImage.stbi_image_free;
 import static org.lwjgl.stb.STBImage.stbi_load_from_memory;
+import static org.lwjgl.stb.STBImageResize.stbir_resize_uint8;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.ByteBuffer;
 import java.nio.IntBuffer;
 
+import org.lwjgl.opengl.GL;
 import org.lwjgl.system.MemoryUtil;
 
 public class Sprite implements Resource {
@@ -44,7 +46,6 @@ public class Sprite implements Resource {
 	private int texture;
 	
 	public Sprite(String path, int filter, int wrap) throws IOException {
-		long time = System.nanoTime();
 		InputStream stream = ResourceManager.getResourceAsStream(path);
 		ByteBuffer mem = MemoryUtil.memAlloc(stream.available());
 		int length;
@@ -52,6 +53,7 @@ public class Sprite implements Resource {
 			mem.put(BUFFER, 0, length);
 		}
 		mem.flip();
+		stream.close();
 		
 		IntBuffer width = MemoryUtil.memAllocInt(1);
 		IntBuffer height = MemoryUtil.memAllocInt(1);
@@ -65,17 +67,33 @@ public class Sprite implements Resource {
 		MemoryUtil.memFree(height);
 		MemoryUtil.memFree(comp);
 		
+		int w = this.width;
+		int h = this.height;
+		if (!GL.getCapabilities().GL_ARB_texture_non_power_of_two) {
+			w = 1;
+			while (w <= this.width) {
+				w <<= 1; // *= 2
+			}
+			
+			h = 1;
+			while (h <= this.height) {
+				h <<= 1; // *= 2
+			}
+		}
+		ByteBuffer d = MemoryUtil.memAlloc(w * h * 4);
+		stbir_resize_uint8(data, this.width, this.height, 0, d, w, h, 0, 4);
+		stbi_image_free(data);
+		
 		texture = glGenTextures();
 		glBindTexture(GL_TEXTURE_2D, texture);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, filter);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, filter);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, wrap);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, wrap);
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, this.width, this.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w, h, 0, GL_RGBA, GL_UNSIGNED_BYTE, d);
 		glBindTexture(GL_TEXTURE_2D, current);
 		
-		stbi_image_free(data);
-		System.out.println("SCORE (lower is better): " + (System.nanoTime() - time) / 1e6);
+		MemoryUtil.memFree(d);
 	}
 	public int getWidth() {
 		return width;
